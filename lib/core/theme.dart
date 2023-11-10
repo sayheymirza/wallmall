427e06479f76b191850250cb7a35bff68425099f
Mirza <sayheymirza@gmail.com>
2023-11-10 13:20:06 +0330

    Add initial theme configuration

---
+++ lib/core/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+  colorScheme: ColorScheme.fromSwatch(
+    primarySwatch: Colors.deepPurple,
+    accentColor: Colors.deepPurpleAccent,
+  ),
+  scaffoldBackgroundColor: Colors.white,
+  useMaterial3: true,
+);
